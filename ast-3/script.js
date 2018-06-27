5a032912d05e0143ac79a3a19dc02421debbbc82
PolyMorpher01 <ayushshta@gmail.com>
2018-06-27 12:05:11 +0545

    added color change in number buttons

--- ast-3/script.js
+++ ast-3/script.js
@@ -4,6 +4,9 @@ var previousButton = document.getElementById("left");
 var imageWrapper = document.getElementsByClassName("image-wrapper");
 var buttonWrapper = document.getElementById("button-number");
 
+$numberButton = document.getElementsByClassName("numberButton");
+
+
 var previousIndex = 0;
 var currentIndex = 0;
 
@@ -20,6 +23,9 @@ var images = [
 
 loadImages();
 
+$numberButton[0].style.backgroundColor = "blue";
+$numberButton[0].style.color = "white";
+
 
 nextButton.onclick = function () {
 
@@ -31,6 +37,15 @@ nextButton.onclick = function () {
         currentIndex = 6;
         return null;
     }
+
+    for( var k=0;k<$numberButton.length;k++){
+        $numberButton[k].style.backgroundColor = "lightskyblue";
+        $numberButton[k].style.color = "black";
+    }
+
+
+    $numberButton[currentIndex].style.backgroundColor = "blue";
+    $numberButton[currentIndex].style.color = "white";
 
     transitionAnimation(currentIndex,previousIndex,"next");
 
@@ -46,6 +61,14 @@ previousButton.onclick = function () {
         currentIndex = 0;
         return null;
     }
+
+    for( var l=0;l<$numberButton.length;l++){
+        $numberButton[l].style.backgroundColor = "lightskyblue";
+        $numberButton[l].style.color = "black";
+    }
+
+    $numberButton[currentIndex].style.backgroundColor = "blue";
+    $numberButton[currentIndex].style.color = "white";
 
     transitionAnimation(currentIndex,previousIndex,"previous");
 
@@ -64,12 +87,21 @@ function loadImages() {
         var newButton = document.createElement('button');
         newButton.innerHTML = i + 1;
         newButton.imagePosition = i;
+        newButton.className = "numberButton";
         buttonWrapper.appendChild(newButton);
 
 
         newButton.onclick = function () {
             currentIndex = this.imagePosition;
             imageWrapper[0].style.marginLeft = currentLeftMargin(currentIndex) + "px";
+
+            for( var j=0;j<$numberButton.length;j++){
+                $numberButton[j].style.backgroundColor = "lightskyblue";
+                $numberButton[j].style.color = "black";
+            }
+
+            this.style.backgroundColor = "blue";
+            this.style.color = "white";
         };
 
     }
