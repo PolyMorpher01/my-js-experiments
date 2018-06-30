083da834595efa4f0bd57f9a46afd4e522ef6d74
PolyMorpher01 <ayushshta@gmail.com>
2018-06-30 10:47:12 +0545

    added bullets and enemy cars

--- ast-9/script.js
+++ ast-9/script.js
@@ -1,25 +1,33 @@
 $mainWrapper = document.getElementById("main-wrapper");
 
 var CONTAINER_PADDING = 30;
-var CONTAINER_LEFT = 0+ CONTAINER_PADDING;
+var CONTAINER_LEFT = 0 + CONTAINER_PADDING;
 var CONTAINER_RIGHT = 360 - CONTAINER_PADDING;
 var CAR_SIZE = 100;
+var CONTAINER_TOP = 0;
+var CONTAINER_BOTTOM = 600;
 
+var GAME_LOOP_INTERVAL = 10;
+
+var BULLET_SIZE = 10;
+var BULLET_SPEED = 4;
+
+var ENEMY_CAR_SPEED = 2;
+
+var BACKGROUND_UPDATE_SPEED = 1;
 
 var newContainer = new Container({
-    $mainWrapper : $mainWrapper
+    $mainWrapper: $mainWrapper
 });
 
 newContainer.init();
-newContainer.start();
-
+newContainer.startGame();
 
 
 // *************Container Class Definition*******************
 function Container(props) {
 
     var self = this;
-
 
 
     var addNewContainer = function () {
@@ -29,7 +37,7 @@ function Container(props) {
     };
 
     var newCar;
-    var addGoodCar = function () {
+    var createGoodCar = function () {
         newCar = new GoodCar({
             x: 130,
             y: 500,
@@ -39,36 +47,136 @@ function Container(props) {
         newCar.init();
     };
 
+    var EnemyCars = [];
+    var createEnemyCars = function () {
 
-    var verticalBackgroundPosition = 1;
-    var BACKGROUND_UPDATE_SPEED = 1;
+        var enemyCar = new EnemyCar({
+            x: CONTAINER_LEFT,
+            y: CONTAINER_TOP,
+            $parent: self.$elem
+        });
 
-    var updateBackgroundPosition = function () {
-        verticalBackgroundPosition += BACKGROUND_UPDATE_SPEED;
-        self.$elem.style.backgroundPosition = "0 "+ verticalBackgroundPosition + "px";
-
+        enemyCar.init();
+        return enemyCar;
     };
 
 
+    var createNewBullet = function () {
+        var newBullet = new Bullet({
+            $parent: self.$elem,
+            x: newCar.x + CAR_SIZE / 2 - BULLET_SIZE / 2, //setting the bullet position middle of the car
+            y: newCar.y - BULLET_SIZE
+        });
+
+        newBullet.init();
+
+        return newBullet;
+    };
 
 
-    self.start = function () {
+    var checkCollision = function (enemyCar, bullet) {
+        var enemyCarTop = enemyCar.y + CAR_SIZE;
+        if (bullet.y < enemyCarTop && (bullet.x >= enemyCar.x && bullet.x <= enemyCar.x + CAR_SIZE)) {
+            console.log("ahhh");
+            console.log(EnemyCars.indexOf(enemyCar));
+            if (enemyCar.destroyEnemyCar()) {
+                EnemyCars.splice(EnemyCars.indexOf(enemyCar), 1)
+            }
+            return true;
+        }
+    };
+
+
+    var verticalBackgroundPosition = 1;
+    var updateBackgroundPosition = function () {
+        verticalBackgroundPosition += BACKGROUND_UPDATE_SPEED;
+        self.$elem.style.backgroundPosition = "0 " + verticalBackgroundPosition + "px";
+
+    };
+
+    var Bullets = [];
+    var addKeyEvents = function () {
+        document.onkeydown = function (event) {
+            if (event.keyCode === 37) {
+                //LEFT
+                if (!(newCar.x < CONTAINER_LEFT + CAR_SIZE)) {
+                    newCar.x -= 100;
+                    newCar.plotPosition();
+                }
+
+            }
+            else if (event.keyCode === 39) {
+                //RIGHT
+                if (!(newCar.x > CONTAINER_RIGHT - CAR_SIZE * 2)) {
+                    newCar.x += 100;
+                    newCar.plotPosition();
+                }
+
+            }
+            else if (event.keyCode === 32) {
+                //SPACE_BAR
+                Bullets.push(createNewBullet());
+            }
+        };
+    };
+
+
+    var updateBullets = function () {
+        for (var i = 0; i < Bullets.length; i++) {
+            Bullets[i].updatePosition();
+
+            if (EnemyCars.length > 0) {
+                var collisionWithEnemy = checkCollision(EnemyCars[0], Bullets[i])
+            }
+
+            //remove bullet
+            if (Bullets[i].y < CONTAINER_TOP || collisionWithEnemy) {
+                Bullets[i].destroyBullet();
+                Bullets.splice(i, 1);
+            }
+
+        }
+        // console.log(Bullets.length);
+    };
+
+
+    var updateEnemyCars = function () {
+        for (var i = 0; i < EnemyCars.length; i++) {
+            EnemyCars[i].updatePosition();
+
+            if (EnemyCars[i].y > CONTAINER_BOTTOM) {
+                EnemyCars[i].enemyHealth = 0;
+                EnemyCars[i].destroyEnemyCar();
+                EnemyCars.splice(i, 1);
+            }
+        }
+    };
+
+    self.startGame = function () {
         self.interval = setInterval(function () {
             updateBackgroundPosition();
 
-        },10);
+            if (Bullets.length > 0) {
+                updateBullets();
+            }
+
+            if (EnemyCars.length > 0) {
+                updateEnemyCars();
+            }
+
+
+        }, GAME_LOOP_INTERVAL);
     };
-
-
 
 
     self.init = function () {
         addNewContainer();
-        addGoodCar();
+        createGoodCar();
+        addKeyEvents();
+        EnemyCars.push(createEnemyCars());
     }
 
 }
-
 
 
 // *************GoodCar Class Definition*******************
@@ -82,7 +190,7 @@ function GoodCar(props) {
     self.$parent = props.$parent;
 
 
-    var plotPosition = function () {
+    self.plotPosition = function () {
         self.$elem.style.left = self.x + "px";
         self.$elem.style.top = self.y + "px";
     };
@@ -90,33 +198,14 @@ function GoodCar(props) {
 
     var addNewCar = function () {
         self.$elem = document.createElement("div");
-        self.$elem.className = "car";
+        self.$elem.className = "car good-car";
         self.$parent.appendChild(self.$elem);
     };
 
+
     self.init = function () {
         addNewCar();
-        plotPosition();
-
-        //Arrow key press event
-        document.onkeydown = function (event) {
-            if (event.keyCode === 37) {
-                //LEFT
-                if(!(self.x<CONTAINER_LEFT+CAR_SIZE)) {
-                    self.x -= 100;
-                    plotPosition();
-                }
-
-            }
-            else if (event.keyCode === 39) {
-                //RIGHT
-                if(!(self.x>CONTAINER_RIGHT-CAR_SIZE*2)) {
-                    self.x += 100;
-                    plotPosition();
-                }
-
-            }
-        };
+        self.plotPosition();
     }
 
 }
@@ -124,5 +213,85 @@ function GoodCar(props) {
 
 //******************Bullet Class Definition***************
 function Bullet(props) {
-    var self = this
+    var self = this;
+
+    self.$parent = props.$parent;
+
+    self.x = props.x;
+    self.y = props.y;
+    self.dy = props.dy || -1;
+
+
+    var addNewBullet = function () {
+        self.$elem = document.createElement("div");
+        self.$elem.className = "bullet";
+        self.$parent.appendChild(self.$elem);
+    };
+
+    var plotPosition = function () {
+        self.$elem.style.left = self.x + "px";
+        self.$elem.style.top = self.y + "px";
+    };
+
+
+    self.updatePosition = function () {
+        self.y = self.y + self.dy * BULLET_SPEED;
+        plotPosition();
+    };
+
+    self.destroyBullet = function () {
+        self.$elem.remove();
+    };
+
+    self.init = function () {
+        addNewBullet();
+        plotPosition();
+    }
+
+}
+
+
+//***********************Enemy Car Class Definition*************
+
+function EnemyCar(props) {
+    var self = this;
+
+    self.x = props.x;
+    self.y = props.y;
+    self.dx = props.dx || 0;
+    self.dy = props.dy || 0;
+    self.$parent = props.$parent;
+    self.enemyHealth = 300;
+
+
+    var plotPosition = function () {
+        self.$elem.style.left = self.x + "px";
+        self.$elem.style.top = self.y + "px";
+    };
+
+
+    var addNewEnemyCar = function () {
+        self.$elem = document.createElement("div");
+        self.$elem.className = "car enemy-car";
+        self.$parent.appendChild(self.$elem);
+    };
+
+    self.updatePosition = function () {
+        self.y = self.y + ENEMY_CAR_SPEED;
+        plotPosition();
+    };
+
+    self.destroyEnemyCar = function () {
+        self.enemyHealth -= 100;
+        if (self.enemyHealth <= 0) {
+            self.$elem.remove();
+            return true;
+        }
+    };
+
+    self.init = function () {
+        addNewEnemyCar();
+        plotPosition();
+    }
+
 }
